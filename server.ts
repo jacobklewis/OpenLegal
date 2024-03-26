fcebdf363ce00fb8c24c5926acbba63bfaf2aed5
Jacob Lewis <Jacob.Lewis2@us.bosch.com>
2024-03-26 12:15:42 -0500

    added rate limiting

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 var express = require("express"),
   cors = require("cors"),
+  rateLimit = require("express-rate-limit"),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require("mongoose"),
@@ -12,6 +13,16 @@ require("dotenv").config();
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO_CONNECTION);
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  // store: ... , // Use an external store for more precise rate limiting
+});
+
+app.use(limiter);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: "1mb" }));
